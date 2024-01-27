cbb7518e13f470849c891b938f614a0f28f46b6d
Oscar Andrés Schwerdt <oscarschwerdt@gmail.com>
2024-01-27 10:58:42 -0300

    First iteration on character controller

---
+++ Player.gd
@@ -0,0 +1,52 @@
+extends CharacterBody2D
+
+enum Drunken_Status{
+	SOBER,
+	LIGHTLY_DRUNKEN,
+	MIDLY_DRUNKEN,
+	HEAVILY_DRUNKEN
+	}
+
+
+@export_category("Default Stats")
+@export var speed : float = 120.0
+var current_speed : float
+@export_range(0.0, 10.0) var acceleration : int = 1
+var current_acceleration : int
+@export_range(0.0, 10.0) var friction : int = 1
+var current_friction : int
+@export var jump_velocity : float = 150.0
+var current_jump_velocity : float
+
+
+const LERP_MULTIPLIER = 0.01
+
+
+func _ready():
+	current_acceleration = acceleration
+	current_friction = friction
+	current_speed = speed
+	current_jump_velocity = jump_velocity
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = -current_jump_velocity
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = lerp(velocity.x, direction * current_speed, current_acceleration * LERP_MULTIPLIER)
+	else:
+		velocity.x = lerp(velocity.x, 0.0, current_friction * LERP_MULTIPLIER)
+
+	move_and_slide()
