2e4bf4c0d2ab3fbb964fe03bd784ec094b7631ec
--global <delfrank94@gmail.com>
2024-01-27 16:43:00 -0300

    trajectory improve

--- Trajectory.gd
+++ Trajectory.gd
@@ -1,6 +1,8 @@
 extends Line2D
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var collsionTest : CharacterBody2D = $CharacterBody2D
+
 func update_trajectory( dir: Vector2, speed: float, delta: float):
 	var max_points =100
 	clear_points()
@@ -9,4 +11,8 @@ func update_trajectory( dir: Vector2, speed: float, delta: float):
 	for i in max_points:
 		add_point(pos)
 		vel.y += gravity * delta
+		var collision = collsionTest.move_and_collide(vel *delta, false, true, true)
+		if collision:
+			return
 		pos += vel * delta
+		collsionTest.position = pos
