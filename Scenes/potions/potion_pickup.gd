a536a88b21387957fab2bd92493b8696c51c69ab
--global <delfrank94@gmail.com>
2024-01-27 19:09:42 -0300

    projectile pickup

--- Scenes/potions/potion_pickup.gd
+++ Scenes/potions/potion_pickup.gd
@@ -1,12 +1,25 @@
 extends Area2D
 
-@export var potion : PackedScene
+enum potionType {
+	JUMPER, TORNADO, BALL, SPLASHER
+}
+@export var potionClass : potionType
+#@export var potion : PackedScene
+@onready var sprite_2d = $Sprite2D
 
 func _ready():
-	if potion == null:
+	if potionClass == null:
 		queue_free()
+	match potionClass:
+		potionType.JUMPER:
+			sprite_2d.modulate = Color.YELLOW
+
+func get_potion():
+	match potionClass:
+		potionType.JUMPER:
+			return preload("res://Scenes/potions/jumper.tscn")
 
 func _on_body_entered(body):
 	if body is Player:
-		body.pickPotion(potion)
+		body.pickPotion(get_potion())
 		queue_free()
