0f0d2275366ab91f01d9cd4a30ddc8ec092bd8df
Oscar Andrés Schwerdt <oscarschwerdt@gmail.com>
2024-01-28 09:53:34 -0300

    changes

--- Scenes/potions/ball.gd
+++ Scenes/potions/ball.gd
@@ -1,5 +1,11 @@
 extends RigidBody2D
 
 @onready var sprite_2d = $Sprite2D
-
+@export var bounces : int = 10
 	
+func _on_area_2d_body_entered(body):
+	if body.position.y < position.y:
+		apply_impulse(Vector2(0,-50))
+	bounces -= 1
+	if bounces == 0:
+		queue_free()
